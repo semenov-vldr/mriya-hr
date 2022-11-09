1946c9ec706e5d7024266b016b807935975e7b35
semenov-vldr <semenov-vldr@users.noreply.github.com>
2022-11-09 15:13:18 +0000

    deploy: d911df029998765a8dffc2297846f32d21ae2a26

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,13 +87,84 @@ if (areas) {
 
 }
 
+// document.addEventListener('DOMContentLoaded', animationSteps);
+//
+//
+// function animationSteps () {
+//   const employment = document.querySelector('.employment');
+//   if (employment) {
+//     const stepNumbers = employment.querySelectorAll('.employment-steps-item__number');
+//
+//   }
+// }
+
+const employment = document.querySelector('.employment');
+
+if (employment) {
+
+  let options = {
+    rootMargin: '0px 0px -20%',
+    threshold: 1
+  };
+
+  const stepNumbers = employment.querySelectorAll('.employment-steps-item__number');
+
+  const line = employment.querySelector('.employment-steps__line--dark');
+  const lineRect = line.getBoundingClientRect();
+  console.log(lineRect)
+
+  const observerStep = new IntersectionObserver(callbackStep, options)
+  const observerLine = new IntersectionObserver(callbackLine, options)
+
+  function callbackStep (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('scroll-center')
+      }
+      else {
+        entry.target.classList.remove('scroll-center')
+      }
+    })
+  }
+
+  function callbackLine (entries) {
+    entries.forEach((entry) => {
+      console.log(entry)
+      let i = 20;
+      if (entry.isIntersecting) {
+        console.log(employment.scrollHeight)
+      }
+      else {
+        entry.target.style.height = `5%`
+      }
+    })
+  }
+
+  stepNumbers.forEach(stepNum => observerStep.observe(stepNum));
+  observerLine.observe(line)
+
+
+
+
+
+
+
+
+//-----------------------
+}
+
+
+
 const header = document.querySelector('.header');
 const headerNav = header.querySelector('.header__nav');
+const page = document.querySelector('.page');
 
 if (headerNav) {
   const burger = header.querySelector('.header__burger');
   burger.addEventListener('click', () => {
     headerNav.classList.toggle('js-active-menu')
+   page.classList.toggle('js-block-scroll');
+    burger.classList.toggle('js-active-menu')
   })
 }
 
