c3e73ac959759edebb6461a7e087a4032c6b0ca5
semenov-vldr <semenov-vldr@users.noreply.github.com>
2022-11-15 14:27:13 +0000

    deploy: 1ce49d12475c642b1dcd3b5b0b8eafa13ae87cf6

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,6 @@ if (areas) {
 
   if (areasItemsLength < startItems + 1) showMore.classList.add('hidden');
 
-
 }
 
 // document.addEventListener('DOMContentLoaded', animationSteps);
@@ -611,17 +610,29 @@ function validForm (forms) {
 if (forms) validForm(forms)
 
 
+const desktopWidth = window.matchMedia('(min-width: 1000.1px)');
+
 const header = document.querySelector('.header');
 const headerNav = header.querySelector('.header__nav');
-const page = document.querySelector('.page');
+const body = document.querySelector('body');
 
 if (headerNav) {
   const burger = header.querySelector('.header__burger');
   burger.addEventListener('click', () => {
-    headerNav.classList.toggle('js-active-menu')
-   page.classList.toggle('js-block-scroll');
-    burger.classList.toggle('js-active-menu')
+    headerNav.classList.toggle('js-active-menu');
+    body.classList.toggle('js-block-scroll');
+    burger.classList.toggle('js-active-menu');
   })
+
+
+  window.addEventListener('resize', () => {
+    if (desktopWidth.matches) {
+      headerNav.classList.remove('js-active-menu');
+      body.classList.remove('js-block-scroll');
+      burger.classList.remove('js-active-menu');
+    }
+  })
+
 }
 
 
@@ -879,6 +890,133 @@ if (stories) {
   };
 
 }
+
+
+
+
+
+
+const vacancies = document.querySelector('.vacancies');
+
+if (vacancies) {
+
+  function addClassFilterActive (items) {
+    items.forEach(item => {
+      item.addEventListener('click', () => {
+        item.classList.toggle('js-filter-active')
+      })
+    })
+  };
+
+  const filterTitles = vacancies.querySelectorAll('.filter__title');
+  addClassFilterActive(filterTitles);
+
+
+
+  // Показать больше
+  const showMoreList = vacancies.querySelectorAll('.filter__button-more');
+  showMoreList.forEach(showMore => {
+    const filterItems = showMore.parentNode.querySelectorAll('.filter__item');
+
+    const startItems = 5;
+    const filterItemsLength = filterItems.length;
+
+    showMore.addEventListener('click', (evt) => {
+      filterItems.forEach(el => el.classList.toggle('js-visible'));
+      showMore.classList.add('visually-hidden');
+    })
+    if (filterItemsLength < startItems + 1) showMore.classList.add('visually-hidden');
+  })
+
+
+
+
+
+  // Показать фильтр на моб. версии по нажатию на кнопку
+
+  const filterSettingBtn = vacancies.querySelector('.vacancies__filter-setting');
+  const filters = vacancies.querySelector('.vacancies__filters');
+  const filtersClose = filters.querySelector('.filters__close');
+
+  const addClassFilterOpen = () => filters.classList.add('js-filter-open');
+  const removeClassFilterOpen = () => filters.classList.remove('js-filter-open');
+
+  filterSettingBtn.addEventListener('click', addClassFilterOpen);
+  filtersClose.addEventListener('click', removeClassFilterOpen);
+
+
+
+
+
+  // Отображение тегов фильтра
+
+  const mobileWidth = window.matchMedia('(max-width: 1000px)');
+
+  const checkboxList = Array.from(filters.querySelectorAll('input[type="checkbox"]'));
+  const filtersActions = filters.querySelector('.filters__actions');
+  const vacanciesFilterContent = vacancies.querySelector('.vacancies-filter-content');
+  const vacanciesFilterContentList = vacanciesFilterContent.querySelector('.vacancies-filter-content__list');
+
+    checkboxList.forEach(checkbox => {
+      checkbox.addEventListener('change', () => {
+        const isChecked = checkboxList.some(item => item.checked);
+        const checkedItems = vacanciesFilterContentList.querySelectorAll('li');
+
+        if (isChecked && mobileWidth.matches) {
+          filtersActions.classList.add('js-filter-active');
+        } else filtersActions.classList.remove('js-filter-active');
+
+        if (checkbox.checked) {
+          const li = document.createElement('li');
+          li.classList.add('vacancies-filter-content__item');
+          const filterDelete = document.createElement('button');
+          li.textContent = checkbox.value;
+          filterDelete.classList.add('vacancies-filter-content__delete');
+          li.appendChild(filterDelete);
+          vacanciesFilterContentList.appendChild(li);
+
+          filterDelete.addEventListener('click', () => {
+            li.remove();
+            checkbox.checked = false;
+          });
+        } else {
+          checkedItems.forEach(checkedItem => {
+            if (checkedItem.textContent === checkbox.value) checkedItem.remove();
+          })
+        }
+
+        if (isChecked) {
+          vacanciesFilterContent.classList.add('js-filter-visible');
+        } else {
+          vacanciesFilterContent.classList.remove('js-filter-visible');
+        }
+
+      const resetFilterBtns = vacancies.querySelectorAll('.vacancies-filter-content__reset');
+        resetFilterBtns.forEach(btn => {
+          btn.addEventListener('click', () => {
+            checkboxList.forEach(checkbox => checkbox.checked = false);
+            vacanciesFilterContent.classList.remove('js-filter-visible');
+            vacanciesFilterContent.querySelector('.vacancies-filter-content__list').replaceChildren();
+          })
+        })
+
+
+      });
+
+
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
