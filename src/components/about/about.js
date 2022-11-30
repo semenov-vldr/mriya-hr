927719b3850fbd05f8a78c23eb8a5a7291c83207
Vladimir <vova2066@yandex.ru>
2022-11-30 10:04:09 +0300

    ref image slider fade

---
+++ src/components/about/about.js
@@ -0,0 +1,56 @@
+{
+
+  let mySwiper;
+
+  const swiperList = document.querySelectorAll('.about__swiper');
+
+  if (swiperList) createSwiper(swiperList);
+
+
+
+  function createSwiper (swiperList) {
+    swiperList.forEach(swiper => {
+
+      mySwiper = new Swiper(swiper, {
+
+        uniqueNavElements: true,
+
+        slidesPerView: 1,
+
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+        },
+
+        effect: "fade",
+
+        // Бесконечная прокрутка
+        //loop: true,
+
+        // Откл функционала, если слайдов меньше, чем нужно
+        //watchOverflow: true,
+
+        //centeredSlides: true,
+
+        // Отступ между слайдами
+        spaceBetween: 24,
+
+        // Стартовый слайд
+        initialSlide: 0,
+
+
+      });
+
+    })
+  };
+
+
+
+
+
+
+
+
+
+
+}
