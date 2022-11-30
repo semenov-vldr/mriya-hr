5c34ddae74f7c18202e86702e4ba575b333192bc
semenov-vldr <semenov-vldr@users.noreply.github.com>
2022-11-30 07:04:53 +0000

    deploy: 927719b3850fbd05f8a78c23eb8a5a7291c83207

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,62 @@
 
 {
+
+  let mySwiper;
+
+  const swiperList = document.querySelectorAll('.about__swiper');
+
+  if (swiperList) createSwiper(swiperList);
+
+
+
+  function createSwiper (swiperList) {
+    swiperList.forEach(swiper => {
+
+      mySwiper = new Swiper(swiper, {
+
+        uniqueNavElements: true,
+
+        slidesPerView: 1,
+
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+        },
+
+        effect: "fade",
+
+        // Бесконечная прокрутка
+        //loop: true,
+
+        // Откл функционала, если слайдов меньше, чем нужно
+        //watchOverflow: true,
+
+        //centeredSlides: true,
+
+        // Отступ между слайдами
+        spaceBetween: 24,
+
+        // Стартовый слайд
+        initialSlide: 0,
+
+
+      });
+
+    })
+  };
+
+
+
+
+
+
+
+
+
+
+}
+
+{
   const data = {
     // класс для всего блока, в котором мы работаем
     classWrapper: '.areas',
